124c50fcbd318733d79361102855d6169a734c66
kirstenalice <kirstenalice@gmail.com>
2017-02-08 22:04:55 -0500

    added AutonRecorder files

---
+++ 2016-2017_Starstruck/Module Test Code/FileIO_Test/include/AutonRecorder.h
@@ -0,0 +1,19 @@
+/*
+ *  AutonRecorder.h
+ *
+ *  Created on: Feb 8, 2017
+ *      Author: Kirsten Meidlinger
+ */
+
+#ifndef AUTONRECORDER_H_
+#define AUTONRECORDER_H_
+
+  /*
+   * This is the library for 750E's Auton Recorder. It reads joystick values and writes them to
+   *   a file in the cortex's memory, and is able to read the file again to replay it as an
+   *   autonomous program. It can work for any game.
+   *
+   * This file should be placed in include and included in main.h
+	 */
+
+#endif
---
+++ 2016-2017_Starstruck/Module Test Code/FileIO_Test/src/AutonRecorder.c
@@ -0,0 +1,15 @@
+/*
+ *  AutonRecorder.c
+ *
+ *  Created on: Feb 8, 2017
+ *      Author: Kirsten Meidlinger
+ */
+
+ #include <main.h>
+
+ /*
+  * This is the file for abstracting Eugene's basic functions.
+  * This file should be placed in the src directory. Please note
+  * that this file will not compile if EugeneLib.h has not been
+  * included properly.
+  */
