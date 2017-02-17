47fc00974d5eb5d71bc7d383c6a9cd14f0313b16
srikayy <srikar.ayyalasomayajula80@gmail.com>
2017-02-17 16:29:49 -0500

    Header/Skeleton for replayer

---
+++ 2016-2017_Starstruck/Module Test Code/FileIO_Test/include/AutonReplayer.h
@@ -0,0 +1,24 @@
+/*
+ *  AutonReplayer.h
+ *
+ *  Created on: Feb 17, 2017
+ *      Author: Mikel Matticoli
+ */
+
+#ifndef AUTONREPLAYER_H_
+
+
+void playAuton(int slot);
+
+
+void stopAuton();
+
+int inputGetAnalog(unsigned char joystick, unsigned char axis);
+
+bool inputGetDigital(unsigned char joystick, unsigned char buttonGroup,
+  unsigned char button);
+
+#define AUTON_REPLAYER_H_
+
+
+#endif /* AUTONREPLAYER_H_ */
---
+++ 2016-2017_Starstruck/Module Test Code/FileIO_Test/src/AutonReplayer.c
@@ -0,0 +1,56 @@
+/*
+ *  AutonReplayer.c
+ *
+ *  Created on: Feb 17, 2017
+ *      Author: Mikel Matticoli
+ */
+
+ #include <main.h>
+
+/*
+ * This file abstracts all of the functionality of 750E's Auton Recorder.
+ *   This file should be placed in the src directory. Note that it won't
+ *   compile without AutonRecorder.h properly included in main.h.
+ */
+
+TaskHandle recordTask;
+Frame *currentFrame = NULLFRAME;
+
+ void replayerLoop();
+
+void replayAuton(int slot) {
+  recordTask = taskCreate(replayerLoop,TASK_DEFAULT_STACK_SIZE,NULL,TASK_PRIORITY_DEFAULT);
+}
+
+void stopAuton() {
+  taskSuspend(recordTask);
+  taskDelete(recordTask);
+  motorStopAll();
+}
+
+void replayerLoop() {
+
+}
+
+int inputGetAnalog(unsigned char joystick, unsigned char axis) {
+  if(isAutonomous()) {
+    if(joystick == 1) {
+      return currentFrame->analog_main[axis - 1];
+    } else {
+      return currentFrame->analog_partner[axis - 1];
+    }
+  } else {
+    return joystickGetAnalog(joystick, axis);
+  }
+}
+
+bool inputGetDigital(unsigned char joystick, unsigned char buttonGroup,
+    unsigned char button) {
+  if(isAutonomous()) {
+
+  } else {
+    return joystickGetDigital(joystick, buttonGroup, button);
+  }
+}
+
+#define AUTON_REPLAYER_H_
