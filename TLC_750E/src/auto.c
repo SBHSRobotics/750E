44a23b55352c41d72a305045e19657808e0f2639
Prad <prad.tinn@gmail.com>
2017-07-16 16:53:22 -0400

    Added actual code

--- TLC_750E/src/auto.c
+++ TLC_750E/src/auto.c
@@ -11,7 +11,14 @@
  */
 
 #include "main.h"
+#include "variables.h"
 
+
+<<<<<<< HEAD
+const int turnSpeed = 1;
+=======
+const int turnSpeed = 180;
+>>>>>>> 00e4c2d3fa23ede678178f28594b597f2d82eda4
 /*
  * Runs the user autonomous code. This function will be started in its own task with the default
  * priority and stack size whenever the robot is enabled via the Field Management System or the
@@ -26,5 +33,95 @@
  * The autonomous task may exit, unlike operatorControl() which should never exit. If it does
  * so, the robot will await a switch to another mode or disable/enable cycle.
  */
+
+//+ = fwd, - = bckwd
+void moveY(int mod) {
+  motorSet(MOTOR_BASE_LEFT1_, mod*127);
+  motorSet(MOTOR_BASE_LEFT2_, mod*127);
+  motorSet(MOTOR_BASE_RIGHT1_, mod*127);
+  motorSet(MOTOR_BASE_RIGHT2_, mod*127);
+}
+//+ = rght, - = lft
+void moveX(int mod) {
+  motorSet(MOTOR_BASE_LEFT1_, mod*127);
+  motorSet(MOTOR_BASE_LEFT2_, -mod*127);
+  motorSet(MOTOR_BASE_RIGHT1_, -mod*127);
+  motorSet(MOTOR_BASE_RIGHT2_, mod*127);
+}
+//+ = rght, - = lft
+void turn(int mod) {
+  motorSet(MOTOR_BASE_LEFT1_, mod*127);
+  motorSet(MOTOR_BASE_LEFT2_, mod*127);
+  motorSet(MOTOR_BASE_RIGHT1_, -mod*127);
+  motorSet(MOTOR_BASE_RIGHT2_, -mod*127);
+}
+<<<<<<< HEAD
+=======
+
+>>>>>>> 00e4c2d3fa23ede678178f28594b597f2d82eda4
+int turnDur(int deg) {
+  return deg/turnSpeed;
+}
+//+ = up, - = dwn
+void lift(int mod) {
+  motorSet(MOTOR_LIFT_LEFT_, mod*127);
+<<<<<<< HEAD
+  motorSet(MOTOR_LIFT_RIGHT, mod*127)
+}
+
+void pince(int mod) {
+  motorSet(MOTOR_PINCE_LEFT_, mod*127);
+  motorSet(MOTOR_PINCE_RIGHT_, mod*127);
+=======
+  motorSet(MOTOR_LIFT_RIGHT_, mod*127);
+}
+
+void pince(int mod) {
+  motorSet(MOTOR_PINCE_, mod*127);
+>>>>>>> 00e4c2d3fa23ede678178f28594b597f2d82eda4
+}
+
+void delayAndStop(int del) {
+  delay(del);
+  stopMotors();
+}
+/*Here is the pinnacle of shitty auton. Due to not using any sensors
+ *in our build, this is our alternative to it
+ */
 void autonomous() {
+<<<<<<< HEAD
+  /*moveY(1);
+  delayAndStop(3200);
+  turn(-1);
+  delayAndStop(turnDur(45));
+  moveY(1);
+  delayAndStop(2200);
+  lift(1);
+  delayAndStop(1000);
+  moveY(1);
+  delayAndStop(500);
+  moveY(-1);
+  delayAndStop(2700);
+  turn(1);
+  delayAndStop(turnDur(45));
+  moveY(1);
+  delayAndStop(3200);*/
+  recorder rec = new recorder();
+=======
+  for (int i = 0; i < 4; i++) {
+    moveY(1);
+    delayAndStop(2500);
+
+    turn(-1);
+    delayAndStop(turnDur(90));
+  }
+
+  lift(1);
+  delayAndStop(1000);
+
+  pince(1);
+  delayAndStop(500);
+  pince(-1);
+  delayAndStop(500);
+>>>>>>> 00e4c2d3fa23ede678178f28594b597f2d82eda4
 }
