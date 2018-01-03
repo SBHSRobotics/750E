5a2488c1634f31702e136aa6ec2a668189c87efb
evelyn-fu <10013355@sbstudents.org>
2018-01-02 22:58:05 -0500

    debugging stuff

--- 2017-2018_In-The-Zone/src/autonRec.c
+++ 2017-2018_In-The-Zone/src/autonRec.c
@@ -4,7 +4,8 @@
 
 void recordAuton(){
 
-  moveFunctions = taskCreate(moveFunctions, TASK_DEFAULT_STACK_SIZE, NULL, TASK_PRIORITY_DEFAULT);
+  //moveFunctions = taskCreate(moveFunctions, TASK_DEFAULT_STACK_SIZE, NULL, TASK_PRIORITY_DEFAULT);
+  printf("TEST");
   FILE * fp;
   if(fileNumber == 1)
     fp = fopen("auton1.txt", "w");
@@ -16,7 +17,7 @@ void recordAuton(){
   int timeInMs = 0;
   bool recAuton = true;
 
-  while(timeInMs <= 15000 && recAuton){
+  /*while(timeInMs <= 15000 && recAuton){
     if(joystickGetDigital(1, 7, JOY_LEFT))
       recAuton = false;
 
@@ -36,6 +37,14 @@ void recordAuton(){
     D6 = joystickGetDigital(1, 6, JOY_DOWN);
     U6 = joystickGetDigital(1, 6, JOY_UP);
 
+    printf("TEST1");
+    drive(A2, A3);
+
+    lift(U6, D6);
+
+    claw(U5, D5);*/
+
+    /*printf("TEST2");
     int temp = A2;
     while(temp < 100) {
       temp *= 10;
@@ -53,13 +62,15 @@ void recordAuton(){
     }
     fprintf(fp, "%d", A3);
     printf("%d", A3);
-    
+
     fprintf(fp, "%d %d %d %d\n", U5, D5, U6, D6);
     printf("%d %d %d %d\n",  U5, D5, U6, D6);
+    printf("TEST3");
     delay(1);
-  }
+    printf("TEST4");*/
+  //}
 
-  taskDelete(moveFunctions);
+  //taskDelete(moveFunctions);
 }
 
 
